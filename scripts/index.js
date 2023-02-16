754e9bcf1ff5eeab822374b141cb4947ff6b132f
Victor <victorshiira4869@gmail.com>
2023-02-16 11:46:40 -0300

    Display the game board's content to the main page

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,23 @@
 const gameBoard = (() => {
-  const board = [];
-  return { board };
+  const board = [
+    [, ,],
+    [, ,],
+    [, ,],
+  ];
+
+  const displayBoard = () => {
+    const boardFields = [...document.querySelectorAll(".board-field")];
+
+    for (let i = 0; i < 3; i++) {
+      for (let x = 0; x < 3; x++) {
+        const fieldValue = document.createElement("span");
+        fieldValue.textContent = board[i][x];
+        boardFields[3 * i + x].appendChild(fieldValue);
+      }
+    }
+  };
+
+  return { board, displayBoard };
 })();
 
 const gameFlow = (() => {
