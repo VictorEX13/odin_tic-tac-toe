8496ff0b3710e8a69a61d5da7c9a66b9e6a2cdb2
Victor <victorshiira4869@gmail.com>
2023-02-17 12:09:22 -0300

    Add marks on clicking the fields of the board

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,65 @@
+const playerFactory = (name, mark) => {
+  return { name, mark };
+};
+
 const gameBoard = (() => {
   const board = [
-    [, ,],
-    [, ,],
-    [, ,],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
 
-  const displayBoard = () => {
-    const boardFields = [...document.querySelectorAll(".board-field")];
+  const addMarks = (row, column, value) => {
+    if (!board[row][column]) {
+      board[row][column] = value;
 
+      gameController.changeCurrentPlayer();
+      gameController.displayBoard();
+    }
+  };
+
+  return { board, addMarks };
+})();
+
+const gameController = (() => {
+  const player1 = playerFactory("Player 1", "X");
+  const player2 = playerFactory("Player 2", "O");
+
+  let currentPlayer = player1;
+
+  const displayBoard = () => {
     for (let i = 0; i < 3; i++) {
       for (let x = 0; x < 3; x++) {
-        const fieldValue = document.createElement("span");
-        fieldValue.textContent = board[i][x];
-        boardFields[3 * i + x].appendChild(fieldValue);
+        if (gameBoard.board[i][x] && !boardFields[3 * i + x].hasChildNodes()) {
+          const fieldValue = document.createElement("span");
+          fieldValue.textContent = gameBoard.board[i][x];
+          boardFields[3 * i + x].appendChild(fieldValue);
+        }
       }
     }
   };
 
-  return { board, displayBoard };
+  const getCurrentPlayer = () => currentPlayer;
+
+  const changeCurrentPlayer = () => {
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+  };
+
+  return { displayBoard, getCurrentPlayer, changeCurrentPlayer };
 })();
 
-const gameFlow = (() => {
-  return {};
-})();
+const boardFields = [...document.querySelectorAll(".board-field")];
 
-const playerFactory = (name) => {
-  return { name };
-};
+boardFields.forEach((field) => {
+  field.addEventListener("click", () => {
+    gameBoard.addMarks(
+      field.getAttribute("data-row"),
+      field.getAttribute("data-col"),
+      gameController.getCurrentPlayer().mark
+    );
+  });
+});
